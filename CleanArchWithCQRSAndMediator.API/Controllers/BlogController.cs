160ab4466775021d39aa74658987845964d95aba
kendrick chukwuka <kendrickchibueze@gmail.com>
2024-05-06 10:01:22 +0100

    feat: fluent validation and API up and running

--- CleanArchWithCQRSAndMediator.API/Controllers/BlogController.cs
+++ CleanArchWithCQRSAndMediator.API/Controllers/BlogController.cs
@@ -1,6 +1,8 @@
-﻿using CleanArchWithCQRSAndMediator.Application.Blogs.Queries.GetBlogs;
-using MediatR;
-using Microsoft.AspNetCore.Http;
+﻿using CleanArchWithCQRSAndMediator.Application.Blogs.Commands.CreateBlog;
+using CleanArchWithCQRSAndMediator.Application.Blogs.Commands.DeleteBlog;
+using CleanArchWithCQRSAndMediator.Application.Blogs.Commands.UpdateBlog;
+using CleanArchWithCQRSAndMediator.Application.Blogs.Queries.GetBlogById;
+using CleanArchWithCQRSAndMediator.Application.Blogs.Queries.GetBlogs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchWithCQRSAndMediator.API.Controllers
@@ -15,5 +17,40 @@ namespace CleanArchWithCQRSAndMediator.API.Controllers
             var blogs = await Mediator.Send(new GetBlogQuery());
             return Ok(blogs);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var blog = await Mediator.Send(new GetBlogByIdQuery() { BlogId = id });
+            if (blog == null) return NotFound();
+            return Ok(blog);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateBlogCommand command)
+        {
+            var createdBlog =  await Mediator.Send(command);
+            // return CreatedAtAction(nameof(GetByIdAsync), new { id = createdBlog.Id }, createdBlog);
+            return Ok(createdBlog);
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBlog(int id, UpdateBlogCommand command)
+        {
+            if (id != command.Id) return BadRequest();
+             await Mediator.Send(command);
+             return NoContent();
+           
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {         
+           var result =  await Mediator.Send(new DeleteBlogCommand { Id = id});
+            if (result == 0) return BadRequest();
+            return NoContent();
+
+        }
+
     }
 }
--- CleanArchWithCQRSAndMediator.Application/ConfigureServices.cs
+++ CleanArchWithCQRSAndMediator.Application/ConfigureServices.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CleanArchWithCQRSAndMediator.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace CleanArchWithCQRSAndMediator.Application
@@ -8,10 +11,13 @@ namespace CleanArchWithCQRSAndMediator.Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(ctg =>
             {
-                ctg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-            });
+            ctg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            ctg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            
+        });
             return services;
         }
     }
